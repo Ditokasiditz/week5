2f980a39a9481bf2e8bd90bf91c8f2652e057fce
Ditokasiditz <kasidit.samad@gmail.com>
2022-08-30 14:28:11 +0700

    input process

--- week5.c
+++ week5.c
@@ -1,6 +1,13 @@
 #include<stdio.h>
 
+//ให้นักศึกษาเขียนโปรแกรมเพื่อรับ string 1 ตัว แล้วเขียนโปรแกรมตรวจสอบว่าเป็น palindrome หรือไม่ 
 int main(){
-    printf("hellp  world ");
+    char f[10],reverse[10];
+    int i;
+
+    scanf("%s", f);
+    printf("%s", f);
+
     return 0;
+
 }
